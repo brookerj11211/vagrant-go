3a13126615431b982fb614e64bb6d821ff69afd4
John Brooker <john.edw.brooker@hp.com>
2015-01-16 15:42:41 -0800

    expand user setup, install git

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -2,13 +2,30 @@
 
 set -x
 
+export DEBIAN_FRONTEND=noninteractive
 VERSION=1.4
 OS=linux
 ARCH=amd64
 
-profile='export PATH=$PATH:/usr/local/go/bin'
+prefix=/usr/local
 
-wget -q https://storage.googleapis.com/golang/go$VERSION.$OS-$ARCH.tar.gz
-tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
-echo $profile > /etc/profile.d/gopath.sh
+# grab go
+if ! [ -e $prefix/go ]; then
 
+    wget -q https://storage.googleapis.com/golang/go$VERSION.$OS-$ARCH.tar.gz
+    tar -C $prefix -xzf go$VERSION.$OS-$ARCH.tar.gz
+fi
+
+# setup user environment variables
+echo "export GOROOT=$prefix/go" > /etc/profile.d/go.sh
+cat << 'EOF' >> /etc/profile.d/go.sh
+
+export GOPATH=$HOME/.gopath
+
+[ -e $GOPATH ] || mkdir -p $GOPATH
+
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+EOF
+
+# not essential but go get depends on it
+which git || apt-get install -y git
